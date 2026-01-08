4f1da2e725cca54e82f2c40f1dd2c49415556d19
Semendyaev Yuriy <varmonger2002@gmail.com>
2026-01-08 20:55:23 +0300

    #0 Геноцид легаси

--- editor/editorCore.js
+++ editor/editorCore.js
@@ -14,6 +14,7 @@ import { WS } from './protocol.js'
 import { createDebugOverlay } from './debug.js'
 import { initDrawing } from './drawing.js'
 import { applyAction } from './actions.js'
+import { initInput } from './input.js' // ✅ ДОБАВЛЕНО
 
 import { setUsers } from './ui/modules/usersPanel.js'
 
@@ -55,6 +56,9 @@ export function initEditor(snapshot) {
     canvas.focus()
 
     restoreCamera()
+
+    // ✅ ПОДКЛЮЧЕНИЕ INPUT
+    initInput(canvas)
 
     // ===== UI =====
     initUI({ role })
